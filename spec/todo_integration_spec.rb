e6b5562c6f59cbde4434344e33076804e1de10b5
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-09-22 10:03:53 +0100

    integration test updated

--- spec/todo_integration_spec.rb
+++ spec/todo_integration_spec.rb
@@ -1,55 +1,59 @@
 require 'todo_list'
 require 'todo1'
 
-RSpec.describe "integration" do
-  describe '#incomplete' do
-  it "returns all non-complete todos" do
-    list = TodoList.new
-    todo = Todo.new("work")
-    list.add(todo)
-    expect(list.incomplete).to eq [todo]
-  end
+RSpec.describe "integration testing" do
+  context 'testing incomplete method' do
+    it "returns all non-complete todos" do
+      list = TodoList.new
+      todo = Todo.new("work")
+      list.add(todo)
+      expect(list.incomplete).to eq [todo]
+    end
 
-  it "returns empty if all todos have been completed" do
-    list = TodoList.new
-    todo = Todo.new("work")
-    list.add(todo)
-    todo.mark_done!
-    expect(list.incomplete).to eq []
-  end
-  
-  it "returns complete todos" do
-    list = TodoList.new
-    todo = Todo.new("work")
-    list.add(todo)
-    todo.mark_done!
-    expect(list.complete).to eq [todo]
-  end
-  it "returns empty if no todos completed" do
-    list = TodoList.new
-    todo = Todo.new("work")
-    list.add(todo)
-    expect(list.complete).to eq []
-  end
-  it "testing multiple todos, some complete/incomplete" do
-    list = TodoList.new
-    todo = Todo.new("work")
-    todo1 = Todo.new("iron")
-    todo.mark_done!
-    list.add(todo)
-    list.add(todo1)
-    expect(list.complete).to eq [todo]
-  end
+    it "returns empty if all todos have been completed" do
+      list = TodoList.new
+      todo = Todo.new("work")
+      list.add(todo)
+      todo.mark_done!
+      expect(list.incomplete).to eq []
+    end
 
-  it "returns all todos as complete" do
-    list = TodoList.new
-    todo = Todo.new("work")
-    todo1 = Todo.new("iron")
-    list.add(todo)
-    list.add(todo1)
-    list.give_up!
-    expect(list.complete).to eq [todo, todo1]
-
+  context 'testing complete method' do
+    it "returns complete todos" do
+      list = TodoList.new
+      todo = Todo.new("work")
+      list.add(todo)
+      todo.mark_done!
+      expect(list.complete).to eq [todo]
+  end
+    it "returns empty if no todos completed" do
+      list = TodoList.new
+      todo = Todo.new("work")
+      list.add(todo)
+      expect(list.complete).to eq []
+    end
+    it "testing multiple todos, some complete/incomplete" do
+      list = TodoList.new
+      todo = Todo.new("work")
+      todo1 = Todo.new("iron")
+      todo.mark_done!
+      list.add(todo)
+      list.add(todo1)
+      expect(list.complete).to eq [todo]
+    end
 end
+  
+  context 'testing give up method' do
+
+    it "returns all todos as complete" do
+      list = TodoList.new
+      todo = Todo.new("work")
+      todo1 = Todo.new("iron")
+      list.add(todo)
+      list.add(todo1)
+      list.give_up!
+      expect(list.complete).to eq [todo, todo1]
+    end
+  end
 end
 end
